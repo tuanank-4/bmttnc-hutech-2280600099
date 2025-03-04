e0a95742a568d7ffbd5896a465edefebc06e7c92
unknown <anh489041@gmail.com>
2025-03-04 10:13:20 +0700

    [add] lap-01 ex01 ex02

---
+++ lap-01/ex02/ex02_01.py
@@ -0,0 +1,5 @@
+#Nhập tên và tuổi từ người dùng
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+#In thông điệp chào mừng với thông tin vừa nhập rint("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
+print("chao mung", ten, "!Ban", tuoi, "tuoi")
